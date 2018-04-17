6d3f8c86fbe54e0622b23c82c5628b7650170824
Beth Palmer <bethpalmerdesigns@gmail.com>
2018-04-17 14:45:19 +0100

    Tidied up the code / removed unnecessaries

--- forms/contact.php
+++ forms/contact.php
@@ -34,16 +34,6 @@ if(isset($_POST['email'])) {
   $email_message .= "Do you have a project budget? ".clean_string($_POST["budget"])."\n";
 
   $email_message .= "Motivation: ".implode(" ", $_POST['check-motivation'])."\n";
-  // $email_message .= "Includes: ".implode(" ", $_POST['check-includes'])."\n";
-  // $email_message .= "Features: ".implode(" ", $_POST['check-features'])."\n";
-
-  // $email_message .= "Images: ".clean_string($_POST["who-images"])."\n";
-  // $email_message .= "Artwork: ".clean_string($_POST["who-artwork"])."\n";
-  // $email_message .= "Copy: ".clean_string($_POST["who-copy"])."\n";
-  // $email_message .= "Logo: ".clean_string($_POST["who-logo"])."\n";
-  // $email_message .= "Downloads: ".clean_string($_POST["who-downloads"])."\n";
-  // $email_message .= "Meta: ".clean_string($_POST["who-meta"])."\n";
-  // $email_message .= "Translations: ".clean_string($_POST["who-translations"])."\n";
 
   $email_message .= "What special features will your site require? ".clean_string($_POST["features"])."\n";
   $email_message .= "Web search words and phrases: ".clean_string($_POST["search"])."\n";
@@ -74,16 +64,10 @@ try
             
     $email_text = "You have a new message from your contact form\n=============================\n";
 
+    // if the variable has content, then include it in the email
     if(isset($email_message)) {
         $emailText .= "$email_message";
     }
-
-    // foreach ($_POST as $key => $value) {
-    //     // If the field exists in the $fields array, include it in the email 
-    //     if (isset($fields[$key])) {
-    //         $emailText .= "$fields[$key]: $value\n";
-    //     }
-    // }
 
     // All the neccessary headers for the email.
     $headers = array('Content-Type: text/plain; charset="UTF-8";',
@@ -115,79 +99,3 @@ if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQ
 else {
     echo $responseArray['message'];
 }
-
-
-
-
-
-// if you are not debugging and don't need error reporting, turn this off by error_reporting(0);
-// error_reporting(E_ALL & ~E_NOTICE);
-
-// try
-// {
-
-//     if(count($_POST) == 0) throw new \Exception('Form is empty');
-            
-//     $emailText = "You have a new message from your contact form\n=============================\n";
-
-//     foreach ($_POST as $key => $value) {
-        // If the field exists in the $fields array, include it in the email 
-    //     if (isset($fields[$key])) {
-    //         $emailText .= "$fields[$key]: $value\n";
-    //         $emailText .= "'Motivations: '. $selectedMotivations";
-    //     }
-    // }
-
-    // All the neccessary headers for the email.
-    // $headers = array('Content-Type: text/plain; charset="UTF-8";',
-    //     'From: ' . $from,
-    //     'Reply-To: ' . $from,
-    //     'Return-Path: ' . $from,
-    // );
-    
-    // Send email
-  /*  mail($sendTo, $subject, $emailText, implode("\n", $headers));
-    header("Location:../services.html");
-}
-else{header("Location:../work.html");
-?><?php
-}
-?>
-
-<!--      $responseArray = array('type' => 'success', 'message' => $okMessage);
- }
- catch (\Exception $e)
- {
-     $responseArray = array('type' => 'danger', 'message' => $errorMessage);
- }
-
-
- if requested by AJAX request return JSON response
- if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
-     $encoded = json_encode($responseArray);
-
-     header('Content-Type: application/json');
-
-     echo $encoded;
- }
- else just display the message
- else {
-     echo $responseArray['message'];
- }
- create email headers
-$headers = 'From: '.$email_from."\r\n".
-'Reply-To: '.$email_from."\r\n" .
-'X-Mailer: PHP/' . phpversion();
-mail($email_to, $email_subject, $email_message, $headers);  -->
-<!-- include your own success html here -->
-<!-- <center>
-<img src="images/logo.png" />
-  <br>
-  <br>
-  Thank you for contacting us.  We will be in touch.<br>
-<br>
- <a href="index.html">HOME</a></center> -->
-
-<!--<?php
-}
-?> -->
